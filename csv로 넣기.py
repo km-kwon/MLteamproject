587414e63d410b72e6b85e0af581ee0aa719fc2b
kyoungmin <lanos5019@ajou.ac.kr>
2022-11-09 00:03:57 +0900

    wacha partial

--- csv로 넣기.py
+++ csv로 넣기.py
@@ -8,6 +8,9 @@ dic = {
     #'리얼스틸': '76460',
     #'리멤버':'191657'
 }
+dic1 = {
+    '건축학개론': 'm5Gvy2d'
+}
 
 f = open(r"C:\Users\kkm\Desktop\대학\2-2\기계학습\팀플\파이썬\data.csv", 'w', encoding='CP949', newline='')
 csvWriter = csv.writer(f)
@@ -32,6 +35,16 @@ for value in dic.values():
             if not reple:
                 continue
             csvWriter.writerow([reple, star_score])
-            
+for value in dic1.values():
+    url= "https://pedia.watcha.com/ko-KR/contents/"+str(value)+"/comments"
+    browser.get(url)
+    browser.implicitly_wait(10)
+    score_boards = browser.find_elements(By.CLASS_NAME,'css-bawlbm')
+    for score_board in score_boards:
+        star = score_board.find_element(By.CLASS_NAME, 'css-yqs4xl')
+        star_score = star.find_element(By.TAG_NAME, 'span').text
+        reple_block = score_board.find_element(By.CLASS_NAME, 'css-1g78l7j')
+        reple = reple_block.find_element(By.TAG_NAME,'span').text
+
 f.close()
     #로딩이 끝날때까지 기다림
