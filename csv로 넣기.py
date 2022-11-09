12f2ab5751d8592de6464d6e45fc98bcc55ec93b
kyoungmin <lanos5019@ajou.ac.kr>
2022-11-09 19:54:47 +0900

    wacha success

--- csv로 넣기.py
+++ csv로 넣기.py
@@ -1,12 +1,15 @@
 from selenium import webdriver as wd
 from selenium.webdriver.common.by import By
 import csv
+from time import sleep
+options= wd.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ["enable-logging"])
 browser = wd.Chrome(executable_path="chromedriver.exe")
-
+browser = wd.Chrome(options=options)
 dic = {
-    '시동': '183876'#,
-    #'리얼스틸': '76460',
-    #'리멤버':'191657'
+    '시동': '183876',
+    '리얼스틸': '76460',
+    '리멤버':'191657'
 }
 dic1 = {
     '건축학개론': 'm5Gvy2d'
@@ -39,12 +42,26 @@ for value in dic1.values():
     url= "https://pedia.watcha.com/ko-KR/contents/"+str(value)+"/comments"
     browser.get(url)
     browser.implicitly_wait(10)
+    before_location = browser.execute_script("return window.pageYOffset")
+    i = 0
+    for i in range(15):
+        browser.execute_script("window.scrollTo(0,{})".format(before_location + 900))
+        sleep(0.5)
+        #browser.implicitly_wait(5)
+        after_location = browser.execute_script("return window.pageYOffset")
+        if before_location == after_location:
+            break
+        else:
+            before_location = browser.execute_script("return window.pageYOffset")
+
     score_boards = browser.find_elements(By.CLASS_NAME,'css-bawlbm')
     for score_board in score_boards:
         star = score_board.find_element(By.CLASS_NAME, 'css-yqs4xl')
         star_score = star.find_element(By.TAG_NAME, 'span').text
         reple_block = score_board.find_element(By.CLASS_NAME, 'css-1g78l7j')
         reple = reple_block.find_element(By.TAG_NAME,'span').text
-
+        reple = reple.strip()
+        star_score = int(float(star_score)*2)
+        csvWriter.writerow([reple, star_score])
 f.close()
     #로딩이 끝날때까지 기다림
